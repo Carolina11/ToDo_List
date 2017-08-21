8afda75a3e9941756afbd047a2725d814db51e88
Carolina11 <williams@syntax61.com>
2017-08-21 15:46:11 -0400

    new Form

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -11,12 +11,48 @@ import './style.css';
 import './styleM.css';
 
 export default class Home extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      listItems:['Buy eggs','Paint deck','Wash car'],
+      inputItem:"Have a great day"
+    }
+  };
+  handleItem = (event) => {
+    this.setState({
+      inputItem:event.target.value
+    })
+  }
+  storeItem = () => {
+    var listItems = this.state.listItems;
+    var inputItem = this.state.inputItem;
+
+    if(inputItem !== "") {
+      listItems.push(inputItem);
+
+      this.setState ({
+        listItems: listItems,
+        inputItem: ""
+      })
+    }
+  }
   render() {
     return (
       <div className="container">
         <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
 
-        //Remove this line and you can start writing your code here.
+        <div className="inputContainer">
+          <input type="text" className="todoInput" value={this.state.inputItem} onChange={this.handleItem} />
+          <input type="submit" className="todoButton" value="Add to list" onClick={this.storeItem} />
+        </div>
+        <div className="todoList">
+          {this.state.listItems.map((item, index) => (
+            <div className="listItem" key={index}>
+              {item}
+            </div>
+          ))
+          }
+        </div>
       </div>
     );
   }
